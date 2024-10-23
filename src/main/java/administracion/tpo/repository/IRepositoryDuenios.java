163c6e5961836ef321e64d824f96e34863719992
Iván Cambiaso <103890671+Cabj1905@users.noreply.github.com>
2024-10-22 22:24:38 -0300

    Delete src directory

--- src/main/java/administracion/tpo/repository/IRepositoryDuenios.java
+++
@@ -1,7 +0,0 @@
-package administracion.tpo.repository;
-
-import administracion.tpo.modelo.Duenios;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IRepositoryDuenios extends JpaRepository<Duenios,Integer> {
-}
